33f411090333da04addd7d679e5fc465e675bb37
root <root@psbu-eganesan-lnx.cisco.com>
2013-10-07 15:23:11 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cssportal::Application.config.secret_key_base = '24dca0f76824dba4fad268cb067bd0a18977c7a47d784a8b88f2a96985886f7694969189394cc11e5506257d127cb44b5072ad1b4a4d99139df4937783165e02'
