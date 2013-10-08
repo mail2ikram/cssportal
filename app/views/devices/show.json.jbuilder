8baff9c31fbc4871693f02c39b0cf80262b9a618
Ikram Mohd <root@psbu-eganesan-lnx.cisco.com>
2013-10-07 17:17:02 -0700

    Added resources

---
+++ app/views/devices/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @device, :type, :description, :user_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Cssportal::Application.routes.draw do
+  resources :devices
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
